2a8dbce80e83bbeefd26c20a4bff153e797d800e
sivasuriyan-p <sivasuriyan58@gmail.com>
2022-08-19 16:28:15 +0530

    tailwind configure in next

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Example() {
+  return (
+    <p className="mx-16 my-16 text-xl font-bold text-green-600 cursor-pointer">
+    Configure Tailwind CSS with Next.JS
+    </p>
+  );
+}
